913940ace3dbe4706368bb07c55b50183146154d
= <=>
2020-12-25 18:23:04 -0300

    Adding web.xml configuration example

--- src/br/com/alura/servlet/HelloWorld.java
+++ src/br/com/alura/servlet/HelloWorld.java
@@ -3,12 +3,16 @@ package br.com.alura.servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/hi")
+/*
+ * This configuration can be made using java annotations or via web.xml file.
+ * This class is configured via web.xml file for example, but the equivalent annotation is written below.
+ * 
+ * @WebServlet(urlPatterns = "/hi")
+ */
 public class HelloWorld extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
